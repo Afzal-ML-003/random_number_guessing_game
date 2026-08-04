7cae07d009a83d1684c0c9f3ed420e0cf6372a3b
Muhammad Afzal <www.deeplearning03@gmail.com>
2026-08-04 23:37:50 +0500

    Increase maximum attempts from 5 to 8

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ import time
 
 st.set_page_config(page_title="Code Guessing Game", page_icon="🔐", layout="centered")
 
-MAX_ATTEMPTS = 5
+MAX_ATTEMPTS = 8
 MIN_CODE = 0
 MAX_CODE = 9  # matches original random.randrange(9) -> 0 to 8
 
